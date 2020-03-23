fa8ffaa950b4a6f6eb915a80b282cdc378402c52
Kibria92 <kibria92@gmail.com>
2020-03-23 14:55:52 +0600

    0.10 here added viewPager Fragment change animations

--- app/src/main/java/com/example/fragmentinsidefragmentpractice/fragment/HomeFragment.java
+++ app/src/main/java/com/example/fragmentinsidefragmentpractice/fragment/HomeFragment.java
@@ -42,6 +42,7 @@ public class HomeFragment extends Fragment {
 
         viewPager = view.findViewById(R.id.viewPager);
         viewPager.setPageTransformer(true, new ZoomOutPageTransformer()); //here view pager animations.
+
         catagoriesTab = view.findViewById(R.id.categories);
         brandsTab = view.findViewById(R.id.brands);
         shopsTab = view.findViewById(R.id.shops);
