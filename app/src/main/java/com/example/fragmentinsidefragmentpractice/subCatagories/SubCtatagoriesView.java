271b87fb55c0e925c614df9e75d3acbb1b1cfce6
Kibria92 <kibria92@gmail.com>
2020-03-31 00:33:00 +0600

    0.16

--- app/src/main/java/com/example/fragmentinsidefragmentpractice/subCatagories/SubCtatagoriesView.java
+++ app/src/main/java/com/example/fragmentinsidefragmentpractice/subCatagories/SubCtatagoriesView.java
@@ -1,8 +1,8 @@
 package com.example.fragmentinsidefragmentpractice.subCatagories;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.captaindroid.tvg.Tvg;
 import com.example.fragmentinsidefragmentpractice.R;
 import com.example.fragmentinsidefragmentpractice.fragment.tabFragment.Catagories;
 import com.example.fragmentinsidefragmentpractice.recyclerAll.CustomAdapter;
@@ -23,29 +22,35 @@ import com.example.fragmentinsidefragmentpractice.recyclerViewClickAndDeviderHun
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class SubCatagoriesForBag extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SubCtatagoriesView extends Fragment {
+    View view;
     RecyclerView recyclerView;
-    List<Integer> subBagimage = new ArrayList<>();
-    List<String> bagItem = new ArrayList<>();
-    TextView textView;
+    List<Integer> image = new ArrayList<>();
+    List<String> item = new ArrayList<>();
 
-    public SubCatagoriesForBag() {
+    public SubCtatagoriesView() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sub_catagories1, container, false);
-        recyclerView = view.findViewById(R.id.recyclerViewBagSubCatagories);
-        textView = view.findViewById(R.id.subCatagoriesTitle);
+        view = inflater.inflate(R.layout.fragment_sub_ctatagories_view, container, false);
+
+
+        //  ((AppCompatActivity) getActivity()).getSupportActionBar().hide();//for hide activity action bar.
+        recyclerView = view.findViewById(R.id.recyclerViewForSubCatagories);
+        // textView = view.findViewById(R.id.subCatagoriesTitle);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));//numberOfColumns
         Toast.makeText(getContext(), "This is click position = " + Catagories.returnPosition, Toast.LENGTH_LONG).show();
-        new CustomSubCatagories(textView, bagItem, subBagimage, Catagories.returnPosition);//set every catarories view
-        CustomAdapter adapter = new CustomAdapter(getContext(), bagItem, subBagimage);
+        new CustomSubCatagoriesModel(item, image, Catagories.returnPosition);//set every catarories view
+        CustomAdapter adapter = new CustomAdapter(getContext(), item, image);
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new MyRecyclerViewDividerItemDecoration(getContext(), GridLayoutManager.HORIZONTAL, 16));
         recyclerView.addItemDecoration(new MyRecyclerViewDividerItemDecoration(getContext(), GridLayoutManager.VERTICAL, 20));
